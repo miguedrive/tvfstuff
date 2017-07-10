92c1b26e1a34f761a57ed245443fe7ecd8eaa98d
Miguel Casas <miguee.casas@gmail.com>
2017-07-10 14:30:29 -0300

    Trabajo al 60%

--- load.php
+++ load.php
@@ -1,20 +1,36 @@
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+<!--Cargo CSS-->
+
+<link rel="stylesheet" href="styles.css">
+
+<div class="topnav navbar" id="myTopnav">
+  <a href="index.html">Home</a>
+  <a href="load.html">Cargar</a>
+  <a href="#delete">Eliminar</a>
+  </div>
+<div align="center"><img src="http://i.imgur.com/GaTBft2.png" width="410" height="170"></div>
+
 <?php
 include 'connection.php';
-//include 'load.html';
-$cargar = $_GET['load1','load2','load3','load4','load5','load6'];
+
+$cargar1 = $_GET['load1'];
+$cargar2 = $_GET['load2'];
+$cargar3 = $_GET['load3'];
+$cargar4 = $_GET['load4'];
+$cargar5 = $_GET['load5'];
+$cargar6 = $_GET['load6'];
+
 
 //INSERT valores del usuario!
-$sql2 = "INSERT INTO descripcion (nombre,sector,descripcion,cantidad,categoria,estado) VALUES ('$load1','$load2','$load3','$load4','$load5','$load6');"
-$result2 = mysqli_query($conn, $sql2);
+ $sql2 = "INSERT INTO objeto (nombre,sector,descripcion,cantidad,categoria,estado)
+ VALUES ('$cargar1','$cargar2','$cargar3','$cargar4','$cargar5','$cargar6')";
+ $result2 = mysqli_query($conn, $sql2);
 
-if ($conn->multi_query($sql2) === TRUE) {
-    echo "Se agregó el nuevo Ítem!";
-} else {
-    echo "Error: " . $sql2 . "<br>" . $conn->error;
-}
-else{
-	echo "No se agregó el ítem."
-}
+echo "Se agregó el ítem!";
 
 $conn->close();
 ?>
--- search.php
+++ search.php
@@ -6,63 +6,6 @@
 <!--Cargo CSS-->
 
 <link rel="stylesheet" href="styles.css">
-
-
-<?php
-//TODO ESTO FUNCIONA! Es JS
-// $link = mysqli_connect("localhost", "root", "casas", "tvfuego");
- 
-// // Check conección a tvf
-// if($link === false){
-//     die("ERROR! No se puede conectar a la base de datos! :(" . mysqli_connect_error());
-// }
- 
-// if(isset($_REQUEST['term'])){
-//     // Consulta a la descripcion de nuestro objeto
-//     $sql = "SELECT * FROM objeto WHERE descripcion LIKE ?";
-    
-//     if($stmt = mysqli_prepare($link, $sql)){
-//         // Bind variables to the prepared statement as parameters
-//         mysqli_stmt_bind_param($stmt, "s", $param_term);
-        
-//         // Set parameters
-//         $param_term = $_REQUEST['term'] . '%';
-        
-//         // check query
-//         if(mysqli_stmt_execute($stmt)){
-//             $result = mysqli_stmt_get_result($stmt);
-            
-//             // Check number of rows in the result set
-//             if(mysqli_num_rows($result) > 0){
-//                 // Fetch result rows as an associative array
-//                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-//                     echo "<p>" . $row["descripcion"] . "</p>";
-//                 }
-//             } else{
-//                 echo "<p>No se encontró el objeto!</p>";
-//             }
-//         } else{
-//             echo "ERROR: No se pudo hacer la consulta $sql. " . mysqli_error($link);
-//         }
-//     }
-     
-//     // Close statement
-//     mysqli_stmt_close($stmt);
-// }
- 
-// // close connection
-// mysqli_close($link);
-
-//SOLO PHP
-
-include 'connection.php';
-// Check connection
-if (!$conn) {
-    die("Fallo la conexión: " . mysqli_connect_error());
-}
-
-?>
-
 <div class="topnav navbar" id="myTopnav">
   <a href="index.html">Home</a>
   <a href="load.html">Cargar</a>
@@ -71,6 +14,14 @@ if (!$conn) {
 <div align="center"><img src="http://i.imgur.com/GaTBft2.png" width="410" height="170"></div>
 
 <?php
+
+include 'connection.php';
+// Check connection
+if (!$conn) {
+    die("Fallo la conexión: " . mysqli_connect_error());
+}
+//Consulta SQL
+
 $sql = "SELECT * FROM objeto WHERE descripcion LIKE '%{$valor}%'";
 $result = mysqli_query($conn, $sql);
 
@@ -81,9 +32,6 @@ if (mysqli_num_rows($result) > 0) {
     	
     	echo " " . $row["nombre"]. " " . $row["descripcion"]. "<br>";
     }
-
-
-
 } else {
     echo "No se encontró ese ítem.";
 }
